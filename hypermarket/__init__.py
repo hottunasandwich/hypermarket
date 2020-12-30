2dcd8473da9c2507b33d30de4201b6a67f32aab5
Eshaq-F <eshagh.farrokhi@email.com>
2020-12-30 22:02:27 +0330

    warehouse manage & orders manage was added

--- hypermarket/__init__.py
+++ hypermarket/__init__.py
@@ -6,6 +6,11 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py', silent=True)
+    app.config.from_mapping(
+        DB_NAME="hyper_sabzi",
+        DB_USER="postgres",
+        DB_PASS="eshaq79"
+    )
     if test_config is not None:
         # load the test config if passed in
         app.config.from_mapping(test_config)
