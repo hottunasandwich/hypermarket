5bc66b78be0ace4c02bb916b656d8bbc45676301
Rodrik Vartoomian <rodrik.v4@gmail.com>
2021-01-13 17:09:24 +0330

    added cart and cart approved

--- hypermarket/store/views.py
+++ hypermarket/store/views.py
@@ -8,48 +8,10 @@ from ..db import get_db
 import psycopg2
 import psycopg2.extras
 import json
-# from cryptography.fernet import Fernet
+from datetime import datetime
 
-store_bp = Blueprint('store', __name__, template_folder='templates', static_folder='static')
-
-# def get_user_password(username):
-#     with open(os.path.join(os.path.dirname(__file__), 'users.json')) as file:
-#         users = json.load(file)
-
-#     for user in users:
-#         if user[0] == username:
-#             return user[1]
-
-# def check_authentication(username, password):
-#     # f = Fernet(current_app.config.get('PASS_KEY'))
-#     password_ = get_user_password(username)
-
-#     return str(f.decrypt(bytes(password_, encoding='UTF-8')), encoding='UTF-8') == password or {username: password} in current_app.config.get('ADMIN')
-
-
-# @store_bp.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if 'user' in session:
-#         return redirect(url_for('admin.dashboard'))
-
-#     else:
-#         login_form = LoginForm()
-#         if request.method == 'GET':
-#             return render_template('store/admin/login.html', form=login_form)
-
-#         else:
-#             if login_form.validate_on_submit():
-#                 username = login_form.username.data
-#                 password = login_form.password.data
-#                 if check_authentication(username, password):
-#                     session['user'] = username
-#                     return redirect(url_for('admin.dashboard'))
-
-#                 else:
-#                     flash('نام کاریری یا رمز غبور اشتباه میباشد')
-#                     return render_template('store/admin/login.html', form=login_form)
-
-
+store_bp = Blueprint(
+    'store', __name__, template_folder='templates', static_folder='static')
 
 
 @store_bp.route('/')
@@ -59,9 +21,10 @@ def home():
     # print(g.customer)
     db = get_db()
     with db:
-        
+
         with db.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-            cursor.execute("select category_name,category_id from category where parent_group=0")
+            cursor.execute(
+                "select category_name,category_id from category where parent_group=0")
             categories = [dict(row) for row in cursor.fetchall()]
             # print(categories)
             total_product = {}
@@ -70,16 +33,16 @@ def home():
                 cursor.execute(f"""select DISTINCT ON (product_name) product_name, product_ware.product_id, price, category_name, image_link from product
                                     join category on product.category_id=category.category_id
                                     join product_ware on product.id=product_ware.product_id
-                                    where parent_group={x['category_id']}
+                                    where parent_group={x['category_id']} and product_ware.number>0
                                     order by  product_name, product_date desc, price asc
                                     limit 4;""")
-                each_product = [dict(row) for row in cursor.fetchall()]        
+                each_product = [dict(row) for row in cursor.fetchall()]
                 total_product[x['category_name']] = each_product
 
-            # print(total_product.keys())    
-            
-    return render_template('store/home.html', total_product=total_product)
-    
+            # print(total_product.keys())
+
+    return render_template('store/home.html', total_product=total_product, whole_cart=session['cart'])
+
 
 # print(url_for('login', next='/'))
 @store_bp.route('/category/?name=<category_name>')
@@ -87,14 +50,16 @@ def category_selector(category_name):
     db = get_db()
     with db:
         with db.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-            cursor.execute(f"select category_id from category where category_name like'%{category_name}%';")
+            cursor.execute(
+                f"select category_id from category where category_name like'%{category_name}%';")
             category_num = cursor.fetchone()['category_id']
-            
-            cursor.execute(f"""select parent_group from category where category_id={category_num}""")
+
+            cursor.execute(
+                f"""select parent_group from category where category_id={category_num}""")
             parent = cursor.fetchone()['parent_group']
 
             if not parent:
-            
+
                 cursor.execute(f"""select DISTINCT ON (product_name) product_name, product_ware.product_id, price, category_name, image_link from product
                                     join category on product.category_id=category.category_id
                                     join product_ware on product.id=product_ware.product_id
@@ -112,15 +77,15 @@ def category_selector(category_name):
             with open('hypermarket/store/categories.json') as f:
                 data = json.load(f)
 
-    return render_template('store/category.html', each_product=each_product , category_name=category_name, data=data)
+    return render_template('store/category.html', each_product=each_product, category_name=category_name, data=data, whole_cart=session['cart'])
 
 
-@store_bp.route('/product/<int:product_id>')
+@store_bp.route('/product/<int:product_id>', methods=['GET', 'POST'])
 def product_selector(product_id):
     db = get_db()
     with db:
         with db.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-            cursor.execute(f"""select product_name, image_link, price, description, category_name, category.category_id from product 
+            cursor.execute(f"""select product_name, image_link, price, description, category_name, category.category_id, product_ware.ware_id from product 
                                 join product_ware on product.id=product_ware.product_id
                                 join category on product.category_id=category.category_id
                                 where product.id={product_id} and number>0
@@ -129,63 +94,104 @@ def product_selector(product_id):
             product = cursor.fetchone()
             cursor.execute(f"""select sum(number) from product 
                                 join product_ware on product.id=product_ware.product_id
-                                where product.id={product_id} and number>0
+                                where product.id={product_id} and number>0 and product_ware.ware_id={product['ware_id']}
                                 group by product_id;""")
-            total =  cursor.fetchone()
-            
+            total = cursor.fetchone()
             cat_id = product['category_id']
+
             category_path = []
-            
+
             while True:
-                cursor.execute(f"""select parent_group, category_name from category where category_id = {cat_id}""")
+                cursor.execute(
+                    f"""select parent_group, category_name from category where category_id = {cat_id}""")
                 _all = cursor.fetchone()
                 cat_id, cat_name = _all['parent_group'], _all['category_name']
                 category_path = [cat_name] + category_path
                 if not cat_id:
                     break
 
-            print(category_path)
+    product_id = str(product_id)
+    in_cart = None
+    if product_id in session['cart']:
+        in_cart = session['cart'][product_id][0]
 
-    return render_template('store/product.html' , product=product, total=total, category_path=category_path)
+    if request.method == 'POST':
+        try:
+            client_order_count = int(request.form['count'])
+            if client_order_count > total['sum'] or client_order_count < 1:
+                raise Exception
 
+            session['cart'][product_id] = (
+                client_order_count, product['ware_id'])
+            flash(
+                f'{product["product_name"]} {request.form["count"]}', 'success')
+
+        except ValueError:
+            flash('تعداد کالا ها قابل قبول نیست', 'danger')
+
+        except Exception:
+            flash('مقدار درخواستی شما قابل دسترس نیست', 'danger')
+
+        return redirect(url_for('store.product_selector', product_id=product_id))
+
+    return render_template('store/product.html', product=product, total=total, category_path=category_path, product_id=product_id, in_cart=in_cart, whole_cart=session['cart'])
 
 
 @store_bp.route('/cart')
 def cart_creation():
-    #****************************
+    products = []
+    db = get_db()
+    with db:
+        with db.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+            for product_id, count_ware_id in session['cart'].items():
+                cursor.execute(f"""select product_name, product.id, image_link, price from product 
+                                    join product_ware on product.id=product_ware.product_id
+                                    join category on product.category_id=category.category_id
+                                    where product.id={product_id} and number>0 and product_ware.ware_id={count_ware_id[1]}""")
+                _ = cursor.fetchone()
+                _['count'] = count_ware_id[0]
+                products.append(_)
 
-        # return render_template('store/cart_approve.html')
+    if session['cart']:
+        session['approved_cart'] = True
 
-    return render_template('store/cart.html')
+    return render_template('store/cart.html', products=products, whole_cart=session['cart'])
 
 
 @store_bp.route('/cart/approve', methods=['POST', 'GET'])
 def cart_approve_function():
-    if request.method == 'POST':
-        db = get_db()
-        with db:
-            with db.cursor() as cursor:
-                cursor.execute("""INSERT INTO orders (username,phone_number,address,order_time)
-                VALUES (%s,%s,%s,%s)""", 
-                (request.form['first_name'] +" "+ request.form['last_name'], request.form['phone'], request.form['address'], request.form['date']))
-    # return redirect(url_for('home'))
-    return render_template('store/cart_approve.html')
+    total_price = 0
+    if session.get('approved_cart') and session['cart']:
+        if request.method == 'POST':
+            db = get_db()
+            with db:
+                with db.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+                    product_ware_id = []
+                    for product_id, value in session['cart'].items():
+                        cursor.execute(
+                            f"""select price, id, number from product_ware
+                            where product_id={int(product_id)} and ware_id={value[1]}
+                            """)
+                        _ = cursor.fetchone()
+                        total_price = total_price + (int(value[0]) * int(_['price']))
+                        product_ware_id.append((_['id'], _['number'] - int(value[0])))
 
-    # if request.method == 'POST':
-    #     f = request.files['file']
-    #     try:
-    #         f.save("uploads/" + secure_filename(f.filename))
-    #         return render_template('store/admin/upload.html')
-    #     finally:
-    #         return redirect(url_for('admin.product_manage'))
+                    cursor.execute("""INSERT INTO orders (username,phone_number,address,delivery_time,order_time,total_cost)
+                    VALUES (%s,%s,%s,%s,%s,%s) RETURNING order_id;""",
+                                   (request.form['first_name'] + " " + request.form['last_name'], request.form['phone'], request.form['address'], datetime.now(), datetime.now(), total_price))
+                    order_id = cursor.fetchone()['order_id']
 
+                    for index, o in enumerate(session['cart'].items(), start=0):
+                        cursor.execute("""INSERT INTO cart (product_ware_id, order_id, count)
+                        VALUES (%s,%s,%s)""",
+                                    (product_ware_id[index][0], order_id, o[1][0]))
+                    
+                        cursor.execute("""UPDATE product_ware SET number=%s WHERE id=%s""", (product_ware_id[index][1], product_ware_id[index][0]))
 
+                    session.pop('cart', None)
+                    session.pop('cart_approved', None)
+                    return redirect(url_for('store.home'))
 
+        return render_template('store/cart_approve.html', whole_cart=session['cart'])
 
-# import psycopg2
-# conn = psycopg2.connect("dbname=psycopg_test user=postgres password=reza80reza80")
-
-# cur = conn.cursor()
-# cur.execute("SELECT id, name, address, salary  from COMPANY")
-# rows = cur.fetchall()
-# print(rows)
+    return 'Not approved for checkout'
